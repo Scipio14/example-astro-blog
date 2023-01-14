c86ffc3694585260840a5c51163076ff2903bd40
Federico Macías <fma.abogado@gmail.com>
2023-01-14 12:43:03 -0600

    adding react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,17 @@ import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+
+
+// https://astro.build/config
 export default defineConfig({
-  site:'https://astro-blog.com',
+  site: 'https://astro-blog.com',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), sitemap({
-    filter:(page)=>page!=='https://astro-blog.com/about/'
-  })]
+    filter: page => page !== 'https://astro-blog.com/about/'
+  }), react()]
 });
